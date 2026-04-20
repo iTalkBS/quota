9d9dbf8133b97d21c2fa3852dad9e834d321b4ed
italkbs <lisubscriptione@gmail.com>
2026-04-20 16:42:25 +0800

    Force clean build

--- check-page.js
+++ check-page.js
@@ -1,15 +1,19 @@
 const fs = require('fs');
 const path = require('path');
+
 const filePath = path.join(__dirname, 'app', '(app)', 'documents', '[id]', 'page.tsx');
-let content = fs.readFileSync(filePath, 'utf8');
+const content = fs.readFileSync(filePath, 'utf8');
 const lines = content.split('\n');
 
-lines.splice(419, 0, "            } catch(e) {");
-lines.splice(420, 0, "              window.open('/api/pdf?id=' + quote.id, '_blank')");
-lines.splice(421, 0, "            }");
+console.log('Total lines:', lines.length);
+console.log('');
+console.log('Lines 220-235:');
+for(let i = 219; i < 235; i++) {
+  console.log((i+1) + ': ' + lines[i]);
+}
 
-fs.writeFileSync(filePath, lines.join('\n'));
-console.log('Fixed. Lines 417-423:');
-for(let i = 416; i < 424; i++) {
+console.log('');
+console.log('Lines 1-20:');
+for(let i = 0; i < 20; i++) {
   console.log((i+1) + ': ' + lines[i]);
 }
